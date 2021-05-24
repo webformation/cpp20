79e10e7eaedbc653fb41fef7b353d736574dc5d7
Dominique <dc@webformation.fr>
2021-05-24 18:16:58 +0200

    ajout visibilite des variables et macros d'un module

---
+++ Module_visibilite/Module_visibilite/Module_visibilite.cpp
@@ -0,0 +1,11 @@
+import <iostream>;
+using namespace std;
+import m_personne;
+
+int main()
+{
+    // cout << MAX << "\n"; // inconnu
+    // cout << i1 << "\n";  // inconnu, meme avec extern int i1;
+    cout << i2 << "\n";
+}
+
---
+++ Module_visibilite/Module_visibilite/m_personne.ixx
@@ -0,0 +1,8 @@
+module;
+#include <iostream>
+export module m_personne;
+
+#define MAX 10
+int i1 = 3;
+export int i2 = 30;
+
