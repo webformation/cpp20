494b9c3d50abc857eea457f4e31d5caccd4e26e5
Dominique <dc@webformation.fr>
2022-04-10 11:44:32 +0200

    Mise a jour range pour compilation avec gcc 11

--- DemoRange/DemoRange/DemoRange.cpp
+++ DemoRange/DemoRange/DemoRange.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <ranges>
 using namespace std;
-using namespace std::ranges::views;
+using namespace std::ranges;
 
 
 
@@ -11,7 +11,7 @@ int main()
     vector<int> entiers{ 1,2,3,4,5,6,7,8 };
     auto pair = [](auto x) { return x % 2 == 0; };
     auto carre = [](auto x) {return x * x; };
-    for (auto i : entiers | filter(pair) |transform(carre)) {
+    for (auto i : entiers | views::filter(pair) | views::transform(carre)) {
         cout << i << ' ';
     }
 }
